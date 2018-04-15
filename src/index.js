32934313888de4350923b38144ddb7f4346c78f1
Brandon Dail <Kierkegaurd@gmail.com>
2018-04-15 14:37:55 -0700

    Implement optimized selectors

--- src/index.js
+++ src/index.js
@@ -39,13 +39,118 @@ type ConsumerCallback<T, S> = (ObservedState<S>, Updater<T>) => React$Node;
  * is defined by ObservedState.
  */
 type Selector<T, S> = T => ObservedState<S>;
+type OptimizedSelector<T, S> = { id: number, fn: Selector<T, S> };
+
+// changedBits
+const MAX_SIGNED_31_BIT_INT = 1073741823;
+const DEOPTIMIZED_SELECTOR = 1;
 
 // The default selector is the identity function
 function identityFn<T>(n: T): T {
   return n;
 }
 
+function getObservedBitsForSelector(selector): number {
+  if (typeof selector.observedBits === "number") {
+    return selector.observedBits;
+  }
+  return MAX_SIGNED_31_BIT_INT;
+}
+
+function getObservedBits<T, S>(selector: Selector<T, S>): number {
+  return Array.isArray(selector)
+    ? selector.reduce(
+        (bits, selector) => bits | getObservedBitsForSelector(selector),
+        0
+      )
+    : getObservedBitsForSelector(selector);
+}
+
 export default function createCopyOnWriteState<T>(baseState: T) {
+  // There are 30 unique bit values, allowing for 30 optimized selectors
+  // at any given time. Since these bits can be dynamically allocated and released
+  // as Consumers mount and unmount, respectively, we model it as a stack of constant
+  // values.
+  let selectorBits = [];
+  let bit = 2;
+  while (bit < MAX_SIGNED_31_BIT_INT) {
+    selectorBits.push(bit);
+    bit <<= 1;
+  }
+  // Selectors which are optimizable are queued here when we attempt to optimize a selector,
+  // but there are no more slots available.
+  const optimizationQueue = [];
+  /**
+   * For optimizable selectors, we need to track how many Consumers are currently
+   * referencing it. That way we know whether we can de-optimize it when a consumer
+   * unmounts, freeing up a slot for a new selector.
+   */
+  const selectorReferenceCount = new Map();
+  /**
+   * We also need to know which selectors are current optimized. This may not always
+   * be the same set of selectors in the selectorReferenceCount map, as we want to track
+   * reference count for selectors that are queued to be optimized as well.
+   */
+  const optimizedSelectors = new Set();
+
+  function optimizeSelector(selector) {
+    if (typeof selector.observedBits !== "number") {
+      // An unknown selector, we can never optimize these. Ignore it.
+      return;
+    }
+    let referenceCount = selectorReferenceCount.get(selector) || 0;
+    selectorReferenceCount.set(selector, referenceCount + 1);
+    if (selector.observedBits !== MAX_SIGNED_31_BIT_INT) {
+      // This selector is already optimized
+      return;
+    }
+    if (!selectorBits.length) {
+      // The selector isn't optimized, but there's no more slots. Queue it
+      // up to be optimized.
+      optimizationQueue.push(selector);
+      return;
+    }
+    // The selector isn't optimized and there are open slots. Register it as an
+    // optimized selector!
+    selector.observedBits = selectorBits.pop();
+    optimizedSelectors.add(selector);
+  }
+
+  function deoptimizeSelector(selector) {
+    if (typeof selector.observedBits !== "number") {
+      return;
+    }
+    // This selector was never optimized. It's likely in the queue, waiting to
+    // be optimized. Ignore it for now.
+    if (optimizedSelectors.has(selector)) {
+      return;
+    }
+    let referenceCount = selectorReferenceCount.get(selector);
+    // referenceCount should always exist, and be greather than 0
+    invariant(
+      typeof referenceCount !== "undefined" && referenceCount > 0,
+      "react-copy-write: attempted to deoptimize a selector that was never " +
+        "optimized. This is likey a bug with react-copy-write"
+    );
+    referenceCount--;
+    // No more references to this selector, it should be de-optimized.
+    if (referenceCount === 0) {
+      // Free up a slot so a queued selector can be optimized.
+      selectorBits.push(selector.observedBits);
+      selector.observedBits = DEOPTIMIZED_SELECTOR;
+      selectorReferenceCount.delete(selector);
+      optimizedSelectors.delete(selector);
+      if (optimizationQueue.length) {
+        // TODO we should choose the selector to optimize based on its reference count.
+        const selector = optimizationQueue.pop();
+        optimizeSelector(selector);
+      }
+    } else {
+      // De-prioritize this selector by updating its reduced reference count
+      selectorReferenceCount.set(selector, referenceCount);
+    }
+  }
+
   /**
    * The current state is stored in a closure, shared by the consumers and
    * the provider. Consumers still respect the Provider/Consumer contract
@@ -54,7 +159,16 @@ export default function createCopyOnWriteState<T>(baseState: T) {
   let currentState: T = baseState;
   let providerListener = null;
   // $FlowFixMe React.createContext exists now
-  const State = React.createContext(baseState);
+  const State = React.createContext(baseState, (stale: T, current: T) => {
+    let changedBits = DEOPTIMIZED_SELECTOR;
+    optimizedSelectors.forEach((bits, selector) => {
+      if (selector(stale) !== selector(current)) {
+        changedBits |= bits;
+      }
+    });
+    return changedBits;
+  });
+
   // Wraps immer's produce. Only notifies the Provider
   // if the returned draft has been changed.
   function update(fn: UpdateFn<T>) {
@@ -77,12 +191,23 @@ export default function createCopyOnWriteState<T>(baseState: T) {
    * to calling mutate(...) directly, except you can define it statically,
    * and have any additional arguments forwarded.
    */
-  function createMutator(fn: UpdateFn<T>)  {
+  function createMutator(fn: UpdateFn<T>) {
     return (...args: mixed[]) => {
       update(draft => {
         fn(draft, ...args);
-      })
-    }
+      });
+    };
+  }
+
+  function createSelector<S>(fn: T => S) {
+    // When an optimized selector is created, it isn't initially optimized.
+    // Once it's actually used in a Consumer that mounts, observedBits will be updated
+    // with a bit flag popped off the selectorBits stack. If selectorBits is empty,
+    // that indicates there are already 30 optimized selectors in use, in which case this
+    // selector will be treated as unoptimized. It will be added to a queue of optimizable selectors
+    // that are waiting for some other selector to unmount.
+    fn.observedBits = DEOPTIMIZED_SELECTOR;
+    return fn;
   }
 
   class CopyOnWriteStoreProvider extends React.Component<
@@ -147,13 +272,6 @@ export default function createCopyOnWriteState<T>(baseState: T) {
       selector: identityFn
     };
 
-    getObservedState(state: T, selectors: Selector<T, S>): ObservedState<S> {
-      if (Array.isArray(selectors)) {
-        return selectors.map(fn => fn(state));
-      }
-      return selectors(state);
-    }
-
     consumer = (state: T) => {
       const { children, selector } = this.props;
       const observedState = this.getObservedState(state, selector);
@@ -164,8 +282,30 @@ export default function createCopyOnWriteState<T>(baseState: T) {
       );
     };
 
+    getObservedState(state: T, selectors: Selector<T, S>): ObservedState<S> {
+      if (Array.isArray(selectors)) {
+        return selectors.map(selector => selector(state));
+      }
+      return selectors(state);
+    }
+
+    componentDidMount() {
+      const selectors = [].concat(this.props.selector);
+      selectors.forEach(optimizeSelector);
+    }
+
+    componentWillUnmount() {
+      const selectors = [].concat(this.props.selector);
+      selectors.forEach(deoptimizeSelector);
+    }
+
     render() {
-      return <State.Consumer>{this.consumer}</State.Consumer>;
+      const observedBits = getObservedBits(this.props.selector);
+      return (
+        <State.Consumer unstable_observedBits={observedBits}>
+          {this.consumer}
+        </State.Consumer>
+      );
     }
   }
 
@@ -187,6 +327,7 @@ export default function createCopyOnWriteState<T>(baseState: T) {
     Consumer: CopyOnWriteConsumer,
     Mutator: CopyOnWriteMutator,
     update,
-    createMutator
+    createMutator,
+    createSelector
   };
 }
