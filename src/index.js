9aa05fec68a70c5f78416f22094aa3ea1c370e34
Brandon Dail <Kierkegaurd@gmail.com>
2018-04-18 20:43:13 -0700

    Implement a priority queue

--- src/index.js
+++ src/index.js
@@ -12,10 +12,13 @@
  * by immer and React.createContext.
  *
  * @flow
+ * @format
  */
-import React, { Component } from "react";
-import produce from "immer";
-import invariant from "invariant";
+import React, {Component} from 'react';
+import produce from 'immer';
+import invariant from 'invariant';
+
+import OptimizationQueue from './OptimizationQueue.js';
 
 // Update functions take the current state, mutate it, and return nothing (undefined)
 type UpdateFn<T> = T => void;
@@ -39,10 +42,12 @@ type ConsumerCallback<T, S> = (ObservedState<S>, Updater<T>) => React$Node;
  * is defined by ObservedState.
  */
 type Selector<T, S> = T => ObservedState<S>;
-type OptimizedSelector<T, S> = { id: number, fn: Selector<T, S> };
+type OptimizedSelector<T, S> = {id: number, fn: Selector<T, S>};
 
-// changedBits
-const MAX_SIGNED_31_BIT_INT = 1073741823;
+/**
+ * If a selector is observing DEOPTIMIZED_SELECTOR, it will always
+ * update the consumer
+ */
 const DEOPTIMIZED_SELECTOR = 1;
 
 // The default selector is the identity function
@@ -50,8 +55,19 @@ function identityFn<T>(n: T): T {
   return n;
 }
 
+function createSelector<S>(fn: T => S) {
+  // When an optimized selector is created, it isn't initially optimized.
+  // Once it's actually used in a Consumer that mounts, observedBits will be updated
+  // with a bit flag popped off the selectorBits stack. If selectorBits is empty,
+  // that indicates there are already 30 optimized selectors in use, in which case this
+  // selector will be treated as unoptimized. It will be added to a queue of optimizable selectors
+  // that are waiting for some other selector to unmount.
+  fn.observedBits = DEOPTIMIZED_SELECTOR;
+  return fn;
+}
+
 function getObservedBitsForSelector(selector): number {
-  if (typeof selector.observedBits === "number") {
+  if (typeof selector.observedBits === 'number') {
     return selector.observedBits;
   }
   return DEOPTIMIZED_SELECTOR;
@@ -61,94 +77,47 @@ function getObservedBits<T, S>(selector: Selector<T, S>): number {
   return Array.isArray(selector)
     ? selector.reduce(
         (bits, selector) => bits | getObservedBitsForSelector(selector),
-        0
+        0,
       )
     : getObservedBitsForSelector(selector);
 }
 
-export default function createCopyOnWriteState<T>(baseState: T) {
-  // There are 30 unique bit values, allowing for 30 optimized selectors
-  // at any given time. Since these bits can be dynamically allocated and released
-  // as Consumers mount and unmount, respectively, we model it as a stack of constant
-  // values.
-  let selectorBits = [];
-  let bit = 2;
-  while (bit < MAX_SIGNED_31_BIT_INT) {
-    selectorBits.push(bit);
-    bit <<= 1;
+/**
+ * Used to determine which queued selector can be optimized. Currently runs in O(n)
+ * where n is the number of queued selectors. Ideally there should never be more than
+ * a few dozen optimized selectors, but it's possible users might create 100s or 1000s
+ * if they're defining the selector as an instance property. With that in mind, it might
+ * be worth using a priority heap to get O(log n) performance.
+ */
+function getHighestPrioritySelector(queue, referenceCountMap) {
+  let highestReferenceCount = 0;
+  let prioritizedSelector;
+  for (let i = 0; i < queue.length; i++) {
+    const selector = queue[i];
+    const referenceCount = referenceCountMap.get(selector);
+    if (referenceCount > highestReferenceCount) {
+      highestReferenceCount = referenceCount;
+      prioritizedSelector = selector;
+    }
   }
-  // Selectors which are optimizable are queued here when we attempt to optimize a selector,
-  // but there are no more slots available.
-  const optimizationQueue = [];
-  /**
-   * For optimizable selectors, we need to track how many Consumers are currently
-   * referencing it. That way we know whether we can de-optimize it when a consumer
-   * unmounts, freeing up a slot for a new selector.
-   */
-  const selectorReferenceCount = new Map();
-  /**
-   * We also need to know which selectors are current optimized. This may not always
-   * be the same set of selectors in the selectorReferenceCount map, as we want to track
-   * reference count for selectors that are queued to be optimized as well.
-   */
-  const optimizedSelectors = new Set();
+}
+
+export default function createCopyOnWriteState<T>(baseState: T) {
+  const queue = new OptimizationQueue();
 
   function optimizeSelector(selector) {
-    if (typeof selector.observedBits !== "number") {
+    if (typeof selector.observedBits !== 'number') {
       // An unknown selector, we can never optimize these. Ignore it.
       return;
     }
-    let referenceCount = selectorReferenceCount.get(selector) || 0;
-    selectorReferenceCount.set(selector, referenceCount + 1);
-    if (selector.observedBits !== DEOPTIMIZED_SELECTOR) {
-      // This selector is already optimized
-      return;
-    }
-    if (!selectorBits.length) {
-      // The selector isn't optimized, but there's no more slots. Queue it
-      // up to be optimized.
-      optimizationQueue.push(selector);
-      return;
-    }
-    // The selector isn't optimized and there are open slots. Register it as an
-    // optimized selector!
-    selector.observedBits = selectorBits.pop();
-    optimizedSelectors.add(selector);
+    queue.reference(selector);
   }
 
   function deoptimizeSelector(selector) {
-    if (typeof selector.observedBits !== "number") {
+    if (typeof selector.observedBits !== 'number') {
       return;
     }
-    // This selector was never optimized. It's likely in the queue, waiting to
-    // be optimized. Ignore it for now.
-    if (!optimizedSelectors.has(selector)) {
-      return;
-    }
-    let referenceCount = selectorReferenceCount.get(selector);
-    // referenceCount should always exist, and be greather than 0
-    invariant(
-      typeof referenceCount !== "undefined" && referenceCount > 0,
-      "react-copy-write: attempted to deoptimize a selector that was never " +
-        "optimized. This is likey a bug with react-copy-write"
-    );
-    referenceCount--;
-    // No more references to this selector, it should be de-optimized.
-    if (referenceCount === 0) {
-      // Free up a slot so a queued selector can be optimized.
-      selectorBits.push(selector.observedBits);
-      selector.observedBits = DEOPTIMIZED_SELECTOR;
-      selectorReferenceCount.delete(selector);
-      optimizedSelectors.delete(selector);
-      if (optimizationQueue.length) {
-        // TODO we should choose the selector to optimize based on its reference count.
-        const selector = optimizationQueue.pop();
-        optimizeSelector(selector);
-      }
-    } else {
-      // De-prioritize this selector by updating its reduced reference count
-      selectorReferenceCount.set(selector, referenceCount);
-    }
+    queue.dereference(selector);
   }
 
   /**
@@ -161,7 +130,7 @@ export default function createCopyOnWriteState<T>(baseState: T) {
   // $FlowFixMe React.createContext exists now
   const State = React.createContext(baseState, (stale: T, current: T) => {
     let changedBits = DEOPTIMIZED_SELECTOR;
-    optimizedSelectors.forEach((bits, selector) => {
+    queue.forEach((bits, selector) => {
       if (selector(stale) !== selector(current)) {
         changedBits |= bits;
       }
@@ -177,7 +146,7 @@ export default function createCopyOnWriteState<T>(baseState: T) {
       `update(...): you cannot call update when no CopyOnWriteStoreProvider ` +
         `instance is mounted. Make sure to wrap your consumer components with ` +
         `the returned Provider, and/or delay your update calls until the component ` +
-        `tree is moutned.`
+        `tree is moutned.`,
     );
     const nextState = produce(currentState, fn);
     if (nextState !== currentState) {
@@ -199,20 +168,9 @@ export default function createCopyOnWriteState<T>(baseState: T) {
     };
   }
 
-  function createSelector<S>(fn: T => S) {
-    // When an optimized selector is created, it isn't initially optimized.
-    // Once it's actually used in a Consumer that mounts, observedBits will be updated
-    // with a bit flag popped off the selectorBits stack. If selectorBits is empty,
-    // that indicates there are already 30 optimized selectors in use, in which case this
-    // selector will be treated as unoptimized. It will be added to a queue of optimizable selectors
-    // that are waiting for some other selector to unmount.
-    fn.observedBits = DEOPTIMIZED_SELECTOR;
-    return fn;
-  }
-
   class CopyOnWriteStoreProvider extends React.Component<
-    { children: React$Node },
-    T
+    {children: React$Node},
+    T,
   > {
     state = baseState;
 
@@ -220,7 +178,7 @@ export default function createCopyOnWriteState<T>(baseState: T) {
       invariant(
         providerListener === null,
         `CopyOnWriteStoreProvider(...): There can only be a single ` +
-          `instance of a provider rendered at any given time.`
+          `instance of a provider rendered at any given time.`,
       );
       providerListener = this.updateState;
     }
@@ -244,36 +202,36 @@ export default function createCopyOnWriteState<T>(baseState: T) {
 
   class ConusmerIndirection<S> extends React.Component<{
     children: ConsumerCallback<T, S>,
-    state: ObservedState<S>
+    state: ObservedState<S>,
   }> {
-    shouldComponentUpdate({ state }: { state: ObservedState<S> }) {
+    shouldComponentUpdate({state}: {state: ObservedState<S>}) {
       if (Array.isArray(state)) {
         // Assumes that if nextProps.state is an array, then the this.props.state is also
         // an array.
         const currentState = ((this.props.state: any): Array<S>);
         return state.some(
-          (observedState, i) => observedState !== currentState[i]
+          (observedState, i) => observedState !== currentState[i],
         );
       }
       return this.props.state !== state;
     }
 
     render() {
-      const { children, state } = this.props;
+      const {children, state} = this.props;
       return children(state, update);
     }
   }
 
   class CopyOnWriteConsumer<S> extends React.Component<{
     selector: Selector<T, S>,
-    children: ConsumerCallback<T, S>
+    children: ConsumerCallback<T, S>,
   }> {
     static defaultProps = {
-      selector: identityFn
+      selector: identityFn,
     };
 
     consumer = (state: T) => {
-      const { children, selector } = this.props;
+      const {children, selector} = this.props;
       const observedState = this.getObservedState(state, selector);
       return (
         <ConusmerIndirection state={observedState}>
@@ -315,7 +273,7 @@ export default function createCopyOnWriteState<T>(baseState: T) {
    * state, but doesn't care about what the current state is
    */
   class CopyOnWriteMutator extends React.Component<{
-    children: (Updater<T>) => React$Node
+    children: (Updater<T>) => React$Node,
   }> {
     render() {
       return this.props.children(update);
@@ -328,6 +286,6 @@ export default function createCopyOnWriteState<T>(baseState: T) {
     Mutator: CopyOnWriteMutator,
     update,
     createMutator,
-    createSelector
+    createSelector,
   };
 }
